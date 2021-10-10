b670ea33998aefdb0a9027e2882188f429f11a6a
Krishna-Potter <krishna.vebbox@gmail.com>
2021-10-10 00:10:08 -0700

    krishna
    changes php file format

---
+++ learning.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning | Datareveal</title>
+
+    <!-- Boostrap css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Font family and Icons-->
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <!-- Navigation Section -->
+    <section for="Nav-bar-menu">
+        <div class="row p-4 bg-dark" id="nav-bar" style="padding: 0;margin: 0;">
+            <h2 class="text-center text-light">navbar</h2>
+        </div>
+    </section>
+
+    <!-- Landing Contents -->
+    <section for="Learn-landing-contents">
+        <div class="row" id="learn-landing" style="padding: 0;margin: 0;">
+            <div class="col-lg-4 col-md-4 col-sm-12 ms-lg-5 my-auto" id="learn-landing-image">
+                <img src="images/vision-landing-mg.png" class="img-fluid mt-5 ms-4 ms-sm-0" alt="Datareveal-Vision"
+                    height="100%" width=90%">
+            </div>
+            <div class="col-1"></div>
+            <div class="col-lg-5 col-md-5 col-sm-12 my-auto p-4" id="learn-landing-contents">
+                <h5 id="learn-landing-contents-h5">Datareveal Extract - Vision</h5>
+                <h3 id="learn-landing-contents-h3">Lorem ipsum dolor sit amet. Et fuga totam in dolor reprehenderit quo
+                    repudiandae nobis ut voluptatum modi. Qui velit sapiente et minima maiores aut necessitatibus.</h3>
+            </div>
+        </div>
+    </section>
+
+    <!-- Subpage Contents -->
+    <section for="Learning-services">
+        <div class="container">
+            <div class="row p-3 mx-auto mt-2" id="learn-content" style="margin: 0;padding: 0;">
+                <a href="#" id="learn-content-a">Datareveal</a>
+                <h2 id="learn-content-h2">Learning</h2>
+                <p class="p-3" id="learn-content-p">Sed voluptatum harum est aliquam nesciunt ut maiores dolores? Ut
+                    quia
+                    eligendi ut
+                    tempora quas aut perferendis voluptatibus. Est quam quas ut optio quia ea inventore ipsum</p>
+                <div class="col-lg-6 col-md-6 col-sm-12 p-2" id="learn-content-one">
+                    <img src="images/learning-1.svg" alt="learning-Datareavel"><span class="ps-2"
+                        id="learn-content-span"> Cognitive
+                        Reasoning Systems
+                        and
+                        Process Automation</span>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 mx-sm-auto p-2" id="learn-content-two">
+                    <img src="images/learning-2.svg" alt="learning-Datareavel"><span class="ps-2"
+                        id="learn-content-span"> Supervised
+                        and Unsupervised Classification and Clustering</span>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 mx-sm-auto mt-3 p-2" id="learn-content-three">
+                    <img src="images/learning-3.svg" alt="learning-Datareavel"><span class="ps-2"
+                        id="learn-content-span"> Predictive
+                        Modeling and Time-Series Forecasting</span>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 mx-sm-auto mt-3 p-2" id="learn-content-four">
+                    <img src="images/learning-4.svg" alt="learning-Datareavel"><span class="ps-2"
+                        id="learn-content-span">
+                        Prescriptive Analytics and Decision Optimization</span>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Parellex Section -->
+    <section for="Parellex">
+        <div class="row p-4" id="parallax" style="margin: 0;padding: 0;">
+            <h3 id="parallax-h3"> Lorem ipsum dolor sit amet. In odit architecto id explicabo tenetur aut
+                galisum omnis aut eius praesentium rem architecto sint non dicta mollitia eum nisi veniam.</h3>
+        </div>
+    </section>
+
+    <!-- Contents Section -->
+    <section for="Contents">
+        <div class="container">
+            <div class="row mt-5 mb-5" id="contents-one" style="margin: 0;padding: 0;">
+                <div class="col-lg-5 col-md-5 col-sm-12 p-1" id="content-image">
+                    <img src="images/learn-content-1.jpg" alt="vision-Datareavel" height="100%" width="100%">
+                </div>
+                <div class="col-1"></div>
+                <div class="col-lg-5 col-md-5 col-sm-12 ps-lg-5 my-auto" id="content-subcontent">
+                    <h2 id="content-subcontent-h2">Lorem ipsm!</h2>
+                    <p id="content-subcontent-p">Lorem ipsum dolor sit amet. In odit architecto id explicabo tenetur aut
+                        galisum omnis aut eius praesentium rem architecto sint non dicta mollitia eum nisi veniam. Et
+                        sapiente quisquam in sint culpa nam inventore quam aut ducimus doloremque eos laborum unde et
+                        quia inventore et autem itaque.</p>
+                </div>
+            </div>
+            <div class="row mt-5 mb-5" id="contents-two">
+                <div class="col-lg-5 col-md-5 col-sm-12 ps-lg-5 my-auto d-none d-sm-block" id="content-subcontent">
+                    <h2 id="content-subcontent-h2">Lorem ipsm!</h2>
+                    <p id="content-subcontent-p">Lorem ipsum dolor sit amet. In odit architecto id explicabo tenetur aut
+                        galisum omnis aut eius praesentium rem architecto sint non dicta mollitia eum nisi veniam. Et
+                        sapiente quisquam in sint culpa nam inventore quam aut ducimus doloremque eos laborum unde et
+                        quia inventore et autem itaque.</p>
+                </div>
+                <div class="col-lg-5 col-md-5 col-sm-12 p-1 ms-lg-5" id="content-image">
+                    <img src="images/learn-content-2.jpg" class="ms-lg-5" alt="vision-Datareavel" height="100%"
+                        width="100%">
+                </div>
+                <div class="col-lg-5 col-md-5 col-sm-12 ps-lg-5 my-auto d-block d-sm-none" id="content-subcontent">
+                    <h2 id="content-subcontent-h2">Lorem ipsm!</h2>
+                    <p id="content-subcontent-p">Lorem ipsum dolor sit amet. In odit architecto id explicabo tenetur aut
+                        galisum omnis aut eius praesentium rem architecto sint non dicta mollitia eum nisi veniam. Et
+                        sapiente quisquam in sint culpa nam inventore quam aut ducimus doloremque eos laborum unde et
+                        quia inventore et autem itaque.</p>
+                </div>
+            </div>
+            <div class="row mt-5 mb-5" id="contents-one">
+                <div class="col-lg-5 col-md-5 col-sm-12 p-1" id="content-image">
+                    <img src="images/learn-content-3.jpg" alt="vision-Datareavel" height="100%" width="100%">
+                </div>
+                <div class="col-1"></div>
+                <div class="col-lg-5 col-md-5 col-sm-12 ps-lg-5 my-auto" id="content-subcontent">
+                    <h2 id="content-subcontent-h2">Lorem ipsm!</h2>
+                    <p id="content-subcontent-p">Lorem ipsum dolor sit amet. In odit architecto id explicabo tenetur aut
+                        galisum omnis aut eius praesentium rem architecto sint non dicta mollitia eum nisi veniam. Et
+                        sapiente quisquam in sint culpa nam inventore quam aut ducimus doloremque eos laborum unde et
+                        quia inventore et autem itaque.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Parellex Section -->
+    <section for="Parellex">
+        <div class="row p-4" id="parallax" style="margin: 0;padding: 0;">
+            <h3 id="parallax-h3"> Lorem ipsum dolor sit amet. In odit architecto id explicabo tenetur aut
+                galisum omnis aut eius praesentium rem architecto sint non dicta mollitia eum nisi veniam.</h3>
+        </div>
+    </section>
+
+    <!-- Features Section -->
+    <section for="Subpage-feature-contents">
+        <?php 
+            include "feature.php";
+        ?>
+    </section>
+
+    <!-- Get In Touch Section -->
+    <section for="Get-in-touch">
+        <?php
+            include "get-in-touch.php";
+        ?>
+    </section>
+
+    <!-- Footer Section -->
+    <section for="Footer">
+        <?php
+            include "footer.php";
+        ?>
+    </section>
+
+</body>
+
+</html>
